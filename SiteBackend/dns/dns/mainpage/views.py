9b84f3beb145f591d15d1a9a8b40a48067c71b70
Nikita <Yaniki2003@mail.ru>
2024-01-06 20:45:19 +0700

    fix

--- SiteBackend/dns/dns/mainpage/views.py
+++ SiteBackend/dns/dns/mainpage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 def index(request):
-    return render(request, 'mainpage/index.html')
+    return render(request, 'mainpage/main_page.html')
 
 def account(request):
     return render(request, 'mainpage/index.html')
