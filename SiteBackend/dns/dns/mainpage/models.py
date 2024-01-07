4e26ef4007a14ae1e8b39d70f068b9d1700b259e
Nikita <Yaniki2003@mail.ru>
2024-01-07 17:38:03 +0700

    e

--- SiteBackend/dns/dns/mainpage/models.py
+++ SiteBackend/dns/dns/mainpage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Component(models.Model):
     name = models.CharField('Название', max_length=255)
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
